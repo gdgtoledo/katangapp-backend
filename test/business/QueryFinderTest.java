ce614125a9b1a49f5396b657776b91e989cf1b82
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-04 21:38:47 +0100

    Add a test for scenario when radius is zero, and current location is away from a bus stop

--- test/business/QueryFinderTest.java
+++ test/business/QueryFinderTest.java
@@ -38,6 +38,24 @@ public class QueryFinderTest extends WithApplication{
 		assertThat(results).hasSize(3);
 	}
 
+	@Test
+	public void testFindRoutesWithoutRadiusShouldNotReturnRoutes() {
+		Finder busStopFinder = Mockito.spy(new BusStopsFinder());
+
+		double latitude = 39.862658;
+		double longitude = -4.025088;
+		int radius = 0;
+
+		mockStore(busStopFinder);
+
+		QueryResult queryResult = busStopFinder.findRoutes(
+			latitude, longitude, radius);
+
+		List<BusStopResult> results = queryResult.getResults();
+
+		assertThat(results).hasSize(0);
+	}
+
 	private void mockStore(Finder finder) {
 		Mockito.when(finder.getStore()).thenReturn(new MockBusStopStore());
 	}
