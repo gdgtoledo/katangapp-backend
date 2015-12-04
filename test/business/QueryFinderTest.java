b108476810d6e39e52e94982bbbb8887452ca3e0
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-04 21:24:24 +0100

    Change Store on test time, using a dummy (hardcoded data) implementation of the BusStop store

---
+++ test/business/QueryFinderTest.java
@@ -0,0 +1,45 @@
+package business;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import business.store.MockBusStopStore;
+
+import models.BusStopResult;
+import models.QueryResult;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import play.test.WithApplication;
+
+/**
+ * @author mdelapenya
+ */
+public class QueryFinderTest extends WithApplication{
+
+	@Test
+	public void testFindRoutes() {
+		Finder busStopFinder = Mockito.spy(new BusStopsFinder());
+
+		double latitude = 39.862658;
+		double longitude = -4.025088;
+		int radius = 2000;
+
+		mockStore(busStopFinder);
+
+		QueryResult queryResult = busStopFinder.findRoutes(
+			latitude, longitude, radius);
+
+		List<BusStopResult> results = queryResult.getResults();
+
+		assertThat(results).hasSize(3);
+	}
+
+	private void mockStore(Finder finder) {
+		Mockito.when(finder.getStore()).thenReturn(new MockBusStopStore());
+	}
+
+}
