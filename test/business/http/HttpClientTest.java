07404f71c8180e68fcae8cdabbb1846148d50455
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-09 09:22:20 +0100

    Mock the Http service, so test don't rely on the real service to be executed
    We use an expected response, stored in an HTML file for tests

--- test/business/http/HttpClientTest.java
+++ test/business/http/HttpClientTest.java
@@ -2,9 +2,16 @@ package business.http;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-import play.test.WithApplication;
+import business.IOTestUtils;
+
+import java.io.IOException;
 
 import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import play.test.WithApplication;
+
 
 /**
  * @author mdelapenya
@@ -12,16 +19,27 @@ import org.junit.Test;
 public class HttpClientTest extends WithApplication {
 
 	@Test
-	public void testGet() {
+	public void testGet() throws Exception {
 		String idl = "41";
 		String idp = "P001";
 		String ido = "1.00000";
 
-		HttpClient httpClient = new HttpClient();
+		HttpClient httpClient = Mockito.spy(new HttpClient());
+
+		mockHttpService(httpClient);
 
 		String response = httpClient.get(idl, idp, ido);
 
 		assertThat(response).isNotEmpty();
+	}
+
+	private void mockHttpService(HttpClient client) throws IOException {
+		String mockResponse = IOTestUtils.readFile("sample-P001.html");
+
+		Mockito.when(
+			client.get(
+				Mockito.anyString(), Mockito.anyString(), Mockito.anyString())
+		).thenReturn(mockResponse);
 	}
 
 }
