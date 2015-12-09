2fda23b87e49619da1f04e28b7f82d858d24673f
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-09 11:35:26 +0100

    Support pretty-printing the JSON output of the "/paradas" endPoint
    Adding a query string parameter named 'prettyPrint' (not case-sensitive) it will allow returning a prettified String instead of a plain JSON.

--- app/controllers/Results.java
+++ app/controllers/Results.java
@@ -6,6 +6,13 @@ import business.http.HttpClient;
 
 import models.QueryResult;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
 import play.libs.Json;
 
 import play.mvc.Controller;
@@ -25,7 +32,20 @@ public class Results extends Controller {
         QueryResult queryResult = busStopFinder.findRoutes(
             dLatitude, dLongitude, r);
 
-        return ok(Json.toJson(queryResult));
+        boolean isPrettyPrint = isPrettyPrint();
+
+        JsonNode node = Json.toJson(queryResult);
+
+        if (isPrettyPrint) {
+            try {
+                return ok(prettyPrint(node)).as("application/json");
+            }
+            catch (JsonProcessingException e) {
+                // fall back to default JSON print
+            }
+        }
+
+        return ok(node);
     }
 
     public static Result unauto(String idl, String idp, String ido) {
@@ -34,6 +54,38 @@ public class Results extends Controller {
         String response = httpClient.get(idl, idp, ido);
 
         return ok(response);
+    }
+
+    private static boolean isPrettyPrint() {
+        Map<String, String[]> queryStringParametersMap =
+            request().queryString();
+
+        boolean isPrettyPrint = queryStringParametersMap.containsKey(
+            "prettyPrint");
+
+        if (isPrettyPrint) {
+            final String[] pretties = queryStringParametersMap.get(
+                "prettyPrint");
+
+            if (pretties[0].equalsIgnoreCase("true") ||
+                pretties[0].equalsIgnoreCase("1")) {
+
+               return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static String prettyPrint(JsonNode node)
+        throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        final ObjectWriter objectWriter =
+            mapper.writerWithDefaultPrettyPrinter();
+
+        return objectWriter.writeValueAsString(node);
     }
 
 }
