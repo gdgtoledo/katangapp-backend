193be87967c83c30ff7d6201ee06c016a594852c
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-16 11:12:35 +0100

    Add tests to verify that routes are lexicographically ordered

--- test/models/RouteResultTest.java
+++ test/models/RouteResultTest.java
@@ -4,6 +4,10 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author mdelapenya
  */
@@ -18,11 +22,72 @@ public class RouteResultTest {
 	}
 
 	@Test
+	public void testCompareShouldBeEqualsButOrderedByRouteId_1() {
+		RouteResult one = new RouteResult("L01", 1000);
+		RouteResult two = new RouteResult("L02", 1000);
+
+		List<RouteResult> list = new ArrayList<>();
+
+		list.add(one);
+		list.add(two);
+
+		Collections.sort(list);
+
+		assertThat(list.get(0)).isEqualTo(one);
+		assertThat(list.get(1)).isEqualTo(two);
+	}
+
+	@Test
+	public void testCompareShouldBeEqualsButOrderedByRouteId_2() {
+		RouteResult one = new RouteResult("L01", 1000);
+		RouteResult two = new RouteResult("L02", 1000);
+
+		List<RouteResult> list = new ArrayList<>();
+
+		list.add(two);
+		list.add(one);
+
+		Collections.sort(list);
+
+		assertThat(list.get(0)).isEqualTo(one);
+		assertThat(list.get(1)).isEqualTo(two);
+	}
+
+	@Test
+	public void testCompareShouldBeEqualsButOrderedByRouteId_3() {
+		RouteResult one = new RouteResult("L01", 1000);
+		RouteResult two = new RouteResult("L02", 1000);
+		RouteResult three = new RouteResult("L03", 1000);
+
+		List<RouteResult> list = new ArrayList<>();
+
+		list.add(three);
+		list.add(two);
+		list.add(one);
+
+		Collections.sort(list);
+
+		assertThat(list.get(0)).isEqualTo(one);
+		assertThat(list.get(1)).isEqualTo(two);
+		assertThat(list.get(2)).isEqualTo(three);
+	}
+
+	@Test
 	public void testCompareShouldBeGreaterThan() {
 		RouteResult one = new RouteResult("", 2000);
 		RouteResult two = new RouteResult("", 1000);
 
 		assertThat(one.compareTo(two)).isEqualTo(1);
+
+		List<RouteResult> list = new ArrayList<>();
+
+		list.add(one);
+		list.add(two);
+
+		Collections.sort(list);
+
+		assertThat(list.get(0)).isEqualTo(two);
+		assertThat(list.get(1)).isEqualTo(one);
 	}
 
 	@Test
@@ -31,6 +96,16 @@ public class RouteResultTest {
 		RouteResult two = new RouteResult("", 2000);
 
 		assertThat(one.compareTo(two)).isEqualTo(-1);
+
+		List<RouteResult> list = new ArrayList<>();
+
+		list.add(one);
+		list.add(two);
+
+		Collections.sort(list);
+
+		assertThat(list.get(0)).isEqualTo(one);
+		assertThat(list.get(1)).isEqualTo(two);
 	}
 
 }
