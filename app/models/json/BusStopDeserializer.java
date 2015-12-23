748c914b5c510e66bbd4594593526083ee5429ad
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-23 20:14:16 +0100

    Add javadocs for the BusStop JSON deserializer

--- app/models/json/BusStopDeserializer.java
+++ app/models/json/BusStopDeserializer.java
@@ -10,10 +10,29 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
 
 /**
+ * This class deserialize a BusStop object in a specific manner, not using the
+ * JSON attribute names for default Jackson deserialize process. Instead, it
+ * will use the conversion defined in the deserialize(...) method.
+ *
  * @author mdelapenya
  */
 public class BusStopDeserializer extends JsonDeserializer<BusStop> {
 
+	/**
+	 * Deserializes a JSON object representing a BusStop into a BusStop model.
+	 * It will expect <code>id, address, lat and long</code> attributes in the
+	 * JSON to fill in <code>id, address, latitude and longitude</code> model
+	 * fields, respectively. Fields <code>routeId and order</code> will be
+	 * populated with a null value.
+	 *
+	 * @param jsonParser
+	 * @param deserializationContext
+	 *
+	 * @return a BusStop object represented the JSON
+	 *
+	 * @see BusStop
+	 * @throws IOException if the JSON cannot be deserialized
+	 */
 	@Override
 	public BusStop deserialize(
 			JsonParser jsonParser,
