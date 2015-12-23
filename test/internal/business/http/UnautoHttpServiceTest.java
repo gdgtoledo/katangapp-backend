f1bfcd1079b5b99c1cef71c9a8294521c461b27d
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-23 20:08:20 +0100

    Add tests for the HttpService with invalid params

--- test/internal/business/http/UnautoHttpServiceTest.java
+++ test/internal/business/http/UnautoHttpServiceTest.java
@@ -1,6 +1,7 @@
 package internal.business.http;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
 
 import business.http.HttpService;
 import business.mocks.MockHttpService;
@@ -25,6 +26,59 @@ public class UnautoHttpServiceTest extends WithApplication {
 		String response = httpService.get(idl, idp, ido);
 
 		assertThat(response).isNotEmpty();
+	}
+
+	@Test
+	public void testGetWithEmptyParameters() throws Exception {
+		final HttpService httpService = new MockHttpService("P001");
+
+		try {
+			httpService.get(new String[0]);
+
+			fail();
+		}
+		catch (IllegalArgumentException iae) {
+			assertThat(iae.getMessage()).isEqualTo(
+				"Wrong service invocation: it only accepts three parameters");
+		}
+	}
+
+	@Test
+	public void testGetWithNullParameters() throws Exception {
+		final HttpService httpService = new MockHttpService("P001");
+
+		try {
+			String[] params = null;
+
+			httpService.get(params);
+
+			fail();
+		}
+		catch (IllegalArgumentException iae) {
+			assertThat(iae.getMessage()).isEqualTo(
+				"Wrong service invocation: it only accepts three parameters");
+		}
+	}
+
+	@Test
+	public void testGetWithParameterLengthGreaterThan3ShouldInvokeService()
+		throws Exception {
+
+		String idl = "41";
+		String idp = "P001";
+		String ido = "1.00000";
+
+		final HttpService httpService = new MockHttpService(idp);
+
+		try {
+			httpService.get(idl, idp, ido, "never-mind");
+
+			fail();
+		}
+		catch (IllegalArgumentException iae) {
+			assertThat(iae.getMessage()).isEqualTo(
+				"Wrong service invocation: it only accepts three parameters");
+		}
 	}
 
 }
