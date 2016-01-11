61a70f6bf73db750a98163a9d3a369036f5ec9a5
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-01-11 17:57:58 +0100

    Add interface for pretty printer behaviour

---
+++ app/es/craftsmanship/toledo/katangapp/controllers/PrettyPrinter.java
@@ -0,0 +1,12 @@
+package es.craftsmanship.toledo.katangapp.controllers;
+
+import play.mvc.Result;
+
+/**
+ * @author mdelapenya
+ */
+public interface PrettyPrinter {
+
+	Result prettyPrint();
+
+}
