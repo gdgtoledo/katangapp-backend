b755fe8b246317f0fad21780618ef4a90dbae284
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-03-30 08:51:40 +0200

    Add a functional test for the main end point

--- test/es/craftsmanship/toledo/katangapp/controllers/KatangappFunctionalTest.java
+++ test/es/craftsmanship/toledo/katangapp/controllers/KatangappFunctionalTest.java
@@ -6,7 +6,10 @@ import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
+import es.craftsmanship.toledo.katangapp.controllers.callbacks.BodyContainsMultipleTestCallback;
 import es.craftsmanship.toledo.katangapp.controllers.callbacks.BodyEqualsTestCallback;
+import es.craftsmanship.toledo.katangapp.models.Point;
+import es.craftsmanship.toledo.katangapp.models.TestPointFactory;
 
 import org.junit.Test;
 
@@ -14,6 +17,27 @@ import org.junit.Test;
  * @author mdelapenya
  */
 public class KatangappFunctionalTest {
+
+    @Test
+    public void testMainEndPoint() {
+        int serverPort = 3333;
+
+        Point puertaBisagra = TestPointFactory.getPuertaBisagra();
+
+        String url =
+            "/paradas?lt=" + puertaBisagra.getLatitude() + "&ln=" +
+                puertaBisagra.getLongitude() + "&r=1000";
+
+        String[] expectedMessages = {
+            "distancia", "parada", "latitude", "longitude"};
+
+        running(
+            testServer(serverPort, fakeApplication(inMemoryDatabase())),
+            HTMLUNIT,
+            new BodyContainsMultipleTestCallback(
+                serverPort, url, expectedMessages)
+        );
+    }
 
     @Test
     public void testNotFoundPath() {
