01ae5e071ccad6425194452673ae5f487474e0bc
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-04-26 12:41:29 +0200

    Fix tests

--- test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
+++ test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
@@ -6,13 +6,13 @@ package es.craftsmanship.toledo.katangapp.test;
 public class SpecsContants {
 
 	public static final String BUS_STOP_JSON =
-		"{\"address\":\"Corralillo de San Miguel, Toledo, España\"," +
+		"{\"address\":\"Corralillo de San Miguel\"," +
 			"\"id\":\"P001\",\"latitude\":39.858966," +
 			"\"longitude\":-4.020902,\"order\":\"208.00000\"," +
 			"\"routeId\":\"L94\"}";
 
 	public static final String BUS_STOP_PRETTIFIED_JSON = "{\n" +
-		"  \"address\" : \"Corralillo de San Miguel, Toledo, España\",\n" +
+		"  \"address\" : \"Corralillo de San Miguel\",\n" +
 		"  \"id\" : \"P001\",\n" +
 		"  \"latitude\" : 39.858966,\n" +
 		"  \"longitude\" : -4.020902,\n" +
