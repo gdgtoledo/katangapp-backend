66e8b40e1daac2285c852fe1950ca1a144e43564
manudevelopia <manugarciarodriguez@gmail.com>
2016-05-02 12:29:30 +0200

    [#4] Init controller skeleton with test

---
+++ app/es/craftsmanship/toledo/katangapp/controllers/KatangappFavoriteApplication.java
@@ -0,0 +1,19 @@
+package es.craftsmanship.toledo.katangapp.controllers;
+
+import play.libs.F;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * @author manudevelopia
+ */
+public class KatangappFavoriteApplication extends Controller {
+
+    public F.Promise<Result> favorite(
+        final String idRoute, final String idBusStop, final String idOrder) {
+
+        return null;
+    }
+
+}
---
+++ test/es/craftsmanship/toledo/katangapp/controllers/KatangappFavoriteApplicationTest.java
@@ -0,0 +1,44 @@
+package es.craftsmanship.toledo.katangapp.controllers;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.status;
+
+import es.craftsmanship.toledo.katangapp.mocks.MockController;
+import es.craftsmanship.toledo.katangapp.test.SpecsContants;
+
+import org.junit.Test;
+
+import play.test.WithApplication;
+
+import play.libs.F;
+
+import play.mvc.Result;
+
+/**
+ * @author manudevelopia
+ */
+public class KatangappFavoriteApplicationTest extends WithApplication {
+
+	@Test
+	public void testFavorite() {
+		String idRoute = null;
+		String idBusStop = null;
+		String idOrder = null;
+
+		MockController.mockRequest(true);
+
+		KatangappFavoriteApplication katangappFavoriteApplication =
+			new KatangappFavoriteApplication();
+
+		F.Promise<Result> resultPromise = katangappFavoriteApplication.favorite(
+			idRoute, idBusStop, idOrder);
+
+		Result result = resultPromise.get(SpecsContants.TIMEOUT);
+
+		assertThat(status(result)).isEqualTo(OK);
+		assertThat(contentType(result)).isEqualTo("application/json");
+	}
+
+}
