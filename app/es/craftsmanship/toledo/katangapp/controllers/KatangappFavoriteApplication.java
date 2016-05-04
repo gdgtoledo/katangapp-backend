84d5feaeaccfc87ac438c04a81cfc5d39f24143d
manudevelopia <manugarciarodriguez@gmail.com>
2016-05-04 10:01:29 +0200

    [#4] Implement favorite method logic

--- app/es/craftsmanship/toledo/katangapp/controllers/KatangappFavoriteApplication.java
+++ app/es/craftsmanship/toledo/katangapp/controllers/KatangappFavoriteApplication.java
@@ -1,6 +1,14 @@
 package es.craftsmanship.toledo.katangapp.controllers;
 
+import es.craftsmanship.toledo.katangapp.business.store.Store;
+import es.craftsmanship.toledo.katangapp.internal.controllers.JsonPrettyPrinter;
+import es.craftsmanship.toledo.katangapp.internal.store.KatangappStore;
+import es.craftsmanship.toledo.katangapp.models.BusStop;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.libs.F;
+import play.libs.Json;
 
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -11,7 +19,24 @@ import play.mvc.Result;
 public class KatangappFavoriteApplication extends Controller {
 
     public F.Promise<Result> favorite(final String busStopId) {
-        return null;
+        return F.Promise.promise(
+            new F.Function0<Result>() {
+                @Override
+                public Result apply() throws Throwable {
+                    BusStop busStop = store.getBusStop(busStopId);
+
+                    JsonNode jsonNode = Json.toJson(busStop);
+
+                    PrettyPrinter prettyPrinter = new JsonPrettyPrinter(
+                        request(), jsonNode);
+
+                    return prettyPrinter.prettyPrint();
+                }
+            }
+        );
+
     }
+
+    private Store store = KatangappStore.getInstance();
 
 }
