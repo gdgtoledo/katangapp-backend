317ee8a6bcea3bf39c7f763eb549326bc4414b48
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-06-22 17:34:50 +0200

    [#7] Define an interface to discover services that the application uses

---
+++ app/es/craftsmanship/toledo/katangapp/services/StatusCheckServiceDiscoveryManager.java
@@ -0,0 +1,12 @@
+package es.craftsmanship.toledo.katangapp.services;
+
+import java.util.List;
+
+/**
+ * @author mdelapenya
+ */
+public interface StatusCheckServiceDiscoveryManager {
+
+	List<StatusCheckService> getServices();
+
+}
