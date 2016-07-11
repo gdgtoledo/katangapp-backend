51f0e1a055ad0e7be31a1de78ffdfef4433def76
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-11 08:56:09 +0200

    [#10] Update tests: now the bus stop store must retrieve a bus stop without route information

--- test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
+++ test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
@@ -8,16 +8,16 @@ public class SpecsContants {
 	public static final String BUS_STOP_JSON =
 		"{\"address\":\"Corralillo de San Miguel\"," +
 			"\"id\":\"P001\",\"latitude\":39.858966," +
-			"\"longitude\":-4.020902,\"order\":\"208.00000\"," +
-			"\"routeId\":\"L94\"}";
+			"\"longitude\":-4.020902,\"order\":null," +
+			"\"routeId\":null}";
 
 	public static final String BUS_STOP_PRETTIFIED_JSON = "{\n" +
 		"  \"address\" : \"Corralillo de San Miguel\",\n" +
 		"  \"id\" : \"P001\",\n" +
 		"  \"latitude\" : 39.858966,\n" +
 		"  \"longitude\" : -4.020902,\n" +
-		"  \"order\" : \"208.00000\",\n" +
-		"  \"routeId\" : \"L94\"\n" +
+		"  \"order\" : null,\n" +
+		"  \"routeId\" : null\n" +
 		"}";
 
 	public static final int SERVER_PORT = 3333;
