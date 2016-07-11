bf62513f63f8b103850257534477890cb4df6821
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-11 14:43:39 +0200

    [#10] Update test specs for the @ignore null on bus stops

--- test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
+++ test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
@@ -8,16 +8,13 @@ public class SpecsContants {
 	public static final String BUS_STOP_JSON =
 		"{\"address\":\"Corralillo de San Miguel\"," +
 			"\"id\":\"P001\",\"latitude\":39.858966," +
-			"\"longitude\":-4.020902,\"order\":null," +
-			"\"routeId\":null}";
+			"\"longitude\":-4.020902}";
 
 	public static final String BUS_STOP_PRETTIFIED_JSON = "{\n" +
 		"  \"address\" : \"Corralillo de San Miguel\",\n" +
 		"  \"id\" : \"P001\",\n" +
 		"  \"latitude\" : 39.858966,\n" +
-		"  \"longitude\" : -4.020902,\n" +
-		"  \"order\" : null,\n" +
-		"  \"routeId\" : null\n" +
+		"  \"longitude\" : -4.020902\n" +
 		"}";
 
 	public static final int SERVER_PORT = 3333;
