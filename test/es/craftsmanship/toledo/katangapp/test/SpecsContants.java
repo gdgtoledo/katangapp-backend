55ca60e46e585ac48b29a0f613a96a6866942285
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-12 21:29:53 +0200

    [#10] Update test specs for linkable models

--- test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
+++ test/es/craftsmanship/toledo/katangapp/test/SpecsContants.java
@@ -7,13 +7,16 @@ public class SpecsContants {
 
 	public static final String BUS_STOP_JSON =
 		"{\"address\":\"Corralillo de San Miguel\"," +
-			"\"id\":\"P001\",\"latitude\":39.858966," +
-			"\"longitude\":-4.020902}";
+			"\"id\":\"P001\",\"latitude\":39.858966,\"links\":{\"self\":" +
+			"\"/api/busStops/P001\"},\"longitude\":-4.020902}";
 
 	public static final String BUS_STOP_PRETTIFIED_JSON = "{\n" +
 		"  \"address\" : \"Corralillo de San Miguel\",\n" +
 		"  \"id\" : \"P001\",\n" +
 		"  \"latitude\" : 39.858966,\n" +
+		"  \"links\" : {\n" +
+		"    \"self\" : \"/api/busStops/P001\"\n" +
+		"  },\n" +
 		"  \"longitude\" : -4.020902\n" +
 		"}";
 
