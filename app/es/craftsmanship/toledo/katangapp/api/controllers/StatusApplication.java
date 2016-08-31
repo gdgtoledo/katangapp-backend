ae97efb4b39ff444e1c62d108bea8135a1b2c981
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-08-31 21:38:57 +0200

    [#13] Document /status endpoint

--- app/es/craftsmanship/toledo/katangapp/api/controllers/StatusApplication.java
+++ app/es/craftsmanship/toledo/katangapp/api/controllers/StatusApplication.java
@@ -9,6 +9,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import com.google.inject.Inject;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
 import play.libs.F.Function;
 import play.libs.F.Promise;
 
@@ -17,6 +22,11 @@ import play.mvc.Result;
 /**
  * @author mdelapenya
  */
+@Api(
+	value = "/status",
+	description = "Health check endpoint of the application, which will" +
+		" return current status of all managed services"
+)
 public class StatusApplication extends BaseKatangaApplication {
 
     @Inject
@@ -25,6 +35,29 @@ public class StatusApplication extends BaseKatangaApplication {
             new KatangaServiceDiscoveryManager(httpService);
     }
 
+	@ApiOperation(
+		httpMethod = "GET",
+		notes = "Executes a sanity check against all managed services for the" +
+			" application to work, measuring the elapsed time to perform it." +
+			" Each service will perform the sanity check in different ways," +
+			" and all of them will be decorated with the resulting time of" +
+			" the operation.",
+		produces = "application/json",
+		response = JsonNode.class,
+		responseContainer = "Array",
+		value = "Returns the current status of each managed service for the" +
+			" application to work"
+	)
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				code = 200, message = "Service: OK.",
+				response = Result.class),
+			@ApiResponse(
+				code = 400, message = "Service: KO",
+				response = Result.class),
+		}
+	)
     public Promise<Result> status() {
 		Promise<JsonNode> jsonNodePromise =
 			serviceDiscoveryManager.processServices();
